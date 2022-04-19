7352dc056b96674ad0f32fb631d731e69d7f6324
song rongjie <songrongjie@ledreamer.com>
2022-04-19 17:49:22 +0800

    init

---
+++ src/components/Fullscreen.vue
@@ -0,0 +1,58 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <h1>以下是协议内容</h1>
+  <d-fullscreen v-model="isOpen" :zIndex="20" mode="immersive"  style="overflow:auto">
+    <div class="demo-fullscreen-container">
+      <d-button @click="isOpen = !isOpen">{{ btnContent }}</d-button>
+      <div id="user-protocol">
+        <p v-for="(item,index) in 100">{{`我是第${index}行内容`}}</p>
+      </div>
+    </div>
+  </d-fullscreen>
+</template>
+<script>
+import { ref, watch } from "vue";
+
+export default {
+  setup() {
+    const isOpen = ref(false);
+    const btnContent = ref("全屏");
+
+    watch(isOpen, (newVal) => {
+      if (newVal) {
+        btnContent.value = "退出全屏";
+      } else {
+        btnContent.value = "全屏";
+      }
+    });
+
+    return {
+      isOpen,
+      btnContent,
+    };
+  },
+};
+</script>
+<style>
+.demo-fullscreen-container {
+  margin: 8px;
+}
+#user-protocol {
+  padding: 22px 16px 24px 16px;
+  font-size: 1.4rem;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: rgba(34, 34, 34, 1);
+  line-height: 2rem;
+  over-flow:scroll;
+}
+.demo-fullscreen-container:fullscreen {
+  overflow: auto;
+  height: 100vh;
+}
+.demo-fullscreen-container:-webkit-full-screen {
+  overflow: auto;
+  height: 100vh;
+}
+
+</style>
---
+++ src/components/Status.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>status组件</h1>
+  <div>
+    <h3 class="h3-box">
+      <span>项目状态</span>
+      <d-status>54444444444444444</d-status>
+    </h3>
+    <d-status type="success">Success</d-status>
+    <d-status type="error">Error</d-status>
+    <d-status type="warning">Warning</d-status>
+    <d-status type="initial">Initial</d-status>
+    <d-status type="waiting">Waiting</d-status>
+    <d-status type="running-2222222">Running</d-status>
+    <d-status type="invalid-11111">Invalid</d-status>
+  </div>
+</template>
+<style >
+    .h3-box {
+        display: inline-block;
+    }
+</style>
